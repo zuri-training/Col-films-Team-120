7f88cce75800f5a233aafc686005d7cb82ced0f8
Bolaji Jimoh Adigun <bolajijimoh8@gmail.com>
2022-08-11 13:10:58 +0100

    library and history pages

---
+++ history&library/script.js
@@ -0,0 +1,8 @@
+var moreIcon = document.querySelector(".more-icon");
+var sidebar = document.querySelector(".sidebar");
+
+
+
+moreIcon.onclick = function() {
+  sidebar.classList.toggle("small-sidebar");
+}
