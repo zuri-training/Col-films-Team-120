3861f1120423cd436701c86ee59380b31bf5fdd7
Hillary Ogieleguea <hillariosky123@gmail.com>
2022-08-15 17:02:21 +0100

    update register and upload view

--- members/views.py
+++ members/views.py
@@ -3,24 +3,23 @@ from .forms import RegistrationForm
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from video.models import Video
+from django.urls import reverse
 # Create your views here.
 
 
 def registerView(request):
     form = RegistrationForm()
-
-    print("hello")
     if request.method == "POST":
         form = RegistrationForm(request.POST)
         # print(form.data)
         if form.is_valid():
             form.save()
-            return JsonResponse({"success": "user registered successfully"})
-        else:
-            formData = form.errors.get_json_data()
-            for err in formData:
-                print(formData[err][0]["message"])
-            return JsonResponse(formData)
+            return redirect(reverse("login"))
+        # else:
+        #     formData = form.errors.get_json_data()
+        #     for err in formData:
+        #         print(formData[err][0]["message"])
+        #     return JsonResponse(formData)
 
     return render(request, "registration/register.html", {"form": form})
 
